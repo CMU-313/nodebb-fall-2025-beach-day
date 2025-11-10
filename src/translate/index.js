45d341536c122f9e37e0065d97614bfe455fd636
Streak <ahoney@andrew.cmu.edu>
2025-11-10 19:56:38 +0000

    fixed linter errors

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,13 +5,13 @@
 const translatorApi = module.exports;
 
 // translatorApi.translate = function (postData) {
-// 	return ['is_english',postData];
+// return ['is_english',postData];
 // };
 
 translatorApi.translate = async function (postData) {
 //  Edit the translator URL below
- const TRANSLATOR_API = 'http://host.docker.internal:5000';
- const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
- const data = await response.json();
- return [data.is_english,data.translated_content];
+	const TRANSLATOR_API = 'http://host.docker.internal:5000';
+	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
 };
